a528368d5ff7aa6686ae338ec3d19adf8ccc2c88
Kevin Gucwa <gucwakj@gmail.com>
2016-04-06 14:50:27 -0700

    add qt env variable for high DPI

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,13 @@
 #include <QApplication>
 
 int main(int argc, char *argv[]) {
+	// scale for high DPI
+	qputenv("QT_AUTO_SCREEN_SCALE_FACTOR", "1");
+
+	// create main app
 	QApplication a(argc, argv);
+
+	// create window and show
 	MainWindow w;
 	w.showMaximized();
 
