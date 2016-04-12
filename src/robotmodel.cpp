a69a9b3868864e359c6d20914763d5fc73459417
Kevin Gucwa <gucwakj@gmail.com>
2016-04-12 12:03:20 -0700

    add a couple new challenges

--- src/robotmodel.cpp
+++ src/robotmodel.cpp
@@ -86,8 +86,8 @@ bool robotModel::newRobot(int id, int form, const rs::Pos &p, const rs::Quat &q,
 		_list[row][P_X] = QVariant(p[0]).toString();
 		_list[row][P_Y] = QVariant(p[1]).toString();
 		_list[row][P_Z] = QVariant(p[2]).toString();
-		_list[row][R_PHI] = QVariant(0).toString();
-		_list[row][R_THETA] = QVariant(0).toString();
+		_list[row][R_PHI] = QVariant(asin(q[0]) * 180 * 2 / rs::Pi).toString();
+		_list[row][R_THETA] = QVariant(asin(q[1]) * 180 * 2 / rs::Pi).toString();
 		_list[row][R_PSI] = QVariant(asin(q[2]) * 180 * 2 / rs::Pi).toString();
 		QColor qtc(c[0]*255, c[1]*255, c[2]*255, c[3]*255);
 		_list[row][COLOR] = QString(qtc.name());
